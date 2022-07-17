de34bd4ae4e0b4c3f993ed90c64fea3240833d6a
Siddharth3-14 <sidd.ansh@gmail.com>
2022-07-17 17:42:41 +0530

    new version of adf added

--- Jens_adf.py
+++ Jens_adf.py
@@ -5,37 +5,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def AngleDifferenceRMS(PolarizationAngleArray):
-
     # assure correct data format
     LengthInputArray = len(PolarizationAngleArray)
-    assert ((LengthInputArray % 2) > 0), 'input array should contain an uneven number of pixels'
-    
+    print(LengthInputArray)
+    # assert ((LengthInputArray % 2) > 0), 'input array should contain an uneven number of pixels'
     
     # separate central pixel from rest of data
     PolarizationAngleCentralPixel = PolarizationAngleArray[int(LengthInputArray/2.)]
-    
     PolarizationAngleArray[int(LengthInputArray/2.)] = np.nan
-    
-    
+     
     # calculate angle differences and remove wraps
     AngleDifferenceArray = np.array(PolarizationAngleArray) - PolarizationAngleCentralPixel
-    
     Selector = (AngleDifferenceArray > 90.)
     AngleDifferenceArray[Selector] = np.array(AngleDifferenceArray[Selector]) - 180.
-    
     Selector = (AngleDifferenceArray < -90.)
     AngleDifferenceArray[Selector] = np.array(AngleDifferenceArray[Selector]) + 180.
-    
-    
+   
     # calculate RMS of angle differences
     RMSAngleDifferences = ( np.nanmean(AngleDifferenceArray**2.) )**0.5
-    
-    
     # return result
     return(RMSAngleDifferences)
 
+def test_func(PolarizationAngleArray):
+    LengthInputArray = len(PolarizationAngleArray)
+    print(LengthInputArray)
+    PolarizationAngleCentralPixel = PolarizationAngleArray[int(LengthInputArray/2.)]
+    PolarizationAngleArray[int(LengthInputArray/2.)] = np.nan
+    # calculate angle differences and remove wraps
+    AngleDifferenceArray = np.array(PolarizationAngleArray) - PolarizationAngleCentralPixel
+    Selector = (AngleDifferenceArray > 90.)
+    AngleDifferenceArray[Selector] = np.array(AngleDifferenceArray[Selector]) - 180.
+    Selector = (AngleDifferenceArray < -90.)
+    AngleDifferenceArray[Selector] = np.array(AngleDifferenceArray[Selector]) + 180.
+   
+    # calculate RMS of angle differences
+    RMSAngleDifferences = ( np.nanmean(AngleDifferenceArray**2.) )**0.5
+    return (RMSAngleDifferences)
 
-FileIn = fits.open('FITS_file/OMC_BandE.fits')
+FileIn = fits.open('../FITS_file/OMC_BandE.fits')
 MapStokesI = FileIn[0]
 MapPolAngle = FileIn[11]
 MapPolFlux = FileIn[13]
@@ -44,7 +51,7 @@ MapPolFluxError = FileIn[14]
 MapPolSNR = MapPolFlux.copy()
 MapPolSNR.data[:] = np.nan
 MapPolSNR.data = MapPolFlux.data / MapPolFluxError.data
-FileIn.info()
+# FileIn.info()
 
 
 
@@ -60,10 +67,9 @@ FileIn.info()
 # ### Actual Calculation
 
 # This sets up the calculation and produces output.
-ApertureMask = Ring2DKernel(0, 5)
-# plt.imshow(ApertureMask)
-# plt.show()
-
+ApertureMask = Ring2DKernel(2, 5)
+plt.imshow(ApertureMask)
+plt.show()
 
 
 BlankedMapPolAngle = MapPolAngle.copy()
@@ -72,7 +78,8 @@ BlankedMapPolAngle = MapPolAngle.copy()
 # flag data at low SNR
 Selector = (MapPolSNR.data < 2.)
 BlankedMapPolAngle.data[Selector] = np.nan
-
+# plt.imshow(BlankedMapPolAngle.data)
+# plt.show()
 
 # assure that data are within +/- 90 deg
 Selector = (BlankedMapPolAngle.data > 90.)
@@ -80,6 +87,7 @@ BlankedMapPolAngle.data[Selector] = BlankedMapPolAngle.data[Selector] - 180.
 
 Selector = (BlankedMapPolAngle.data < -90.)
 BlankedMapPolAngle.data[Selector] = BlankedMapPolAngle.data[Selector] + 180.
+
 
 
 # calculate angle differences
@@ -94,10 +102,10 @@ plt.imshow(RMSAngleDifferences, origin='lower')
 plt.colorbar()
 plt.show()
 
-# output to FITS
-MapRMSAngleDifference = MapPolAngle.copy()
-MapRMSAngleDifference.data = RMSAngleDifferences
-MapRMSAngleDifference.writeto('Dummy.fits')
+# # output to FITS
+# MapRMSAngleDifference = MapPolAngle.copy()
+# MapRMSAngleDifference.data = RMSAngleDifferences
+# MapRMSAngleDifference.writeto('Dummy.fits')
 
 
 # At this point the variable `RMSAngleDifferences` contains an array that holds the RMS angle differences between one pixel and its neighbors. It can be written out into a FITS file or similar.
--- test.py
+++ test.py
@@ -22,20 +22,6 @@ def generate_RA_DEC_mesh(hdr):
         RA_delt = hdr.header['CD1_1']
         DEC_delt = hdr.header['CD2_2']
 
-    RA_ref = int(hdr.header['CRPIX1'])
-    DEC_ref = int(hdr.header['CRPIX2'])
-    RA_ref_value = hdr.header['CRVAL1']
-    DEC_ref_value = hdr.header['CRVAL2']
-    RA_axis_len = hdr.header['NAXIS1']
-    DEC_axis_len = hdr.header['NAXIS2']
-
-    RA_axis = np.arange(0,RA_axis_len,1)
-    DEC_axis = np.arange(0,DEC_axis_len,1)
-    RA_axis = RA_ref_value - RA_delt*(RA_ref - RA_axis)
-    DEC_axis = DEC_ref_value + DEC_delt*(DEC_axis - DEC_ref)
-    DEC_grid,RA_grid = np.meshgrid(DEC_axis,RA_axis , sparse=False, indexing='ij')
-
-
     RA_ref = (hdr.header['CRPIX1'])
     DEC_ref = (hdr.header['CRPIX2'])
     RA_ref_value = hdr.header['CRVAL1']
@@ -56,100 +42,212 @@ def generate_RA_DEC_mesh(hdr):
     return DEC_grid,RA_grid
 
 def Calc_l(ra1,dec1,ra2,dec2):
+    """Calc_l
 
+    Find the seperation between two points on the celestial sphere located at points (ra1,dec1) and (ra2,dec2)
+    returns the value in armin
+
+    """
     c1 = SkyCoord(ra1,dec1,unit = 'deg')
     c2 = SkyCoord(ra2,dec2,unit = 'deg')
     sep = c1.separation(c2)
     return sep.arcminute
 
-FITS1 = 'FITS_file/OMC_BandE.fits'
+########## importing and testing the file
+FITS1 = '../FITS_file/OMC_BandE.fits'
 hdul = fits.open(FITS1)
 # print(hdul.info())
-Pol_angle = hdul[11].data
+MapStokesI = hdul[0]
+MapStokesQ = hdul[2]
+MapStokesU = hdul[4]
+MapDebPol = hdul[8]
+MapPolAngle = hdul[11]
+MapPolFlux = hdul[13]
+MapPolFluxError = hdul[14]
 
-# # evt_data = Table(hdul[18].data)
-# # print(evt_data.colnames)
+
+MapPolSNR = MapPolFlux.copy()
+BlankedMapPol = MapDebPol.copy()
+BlankedMapPolAngle = MapPolAngle.copy()
+BlankedMapStokesI = MapStokesI.copy()
+BlankedMapStokesQ = MapStokesQ.copy()
+BlankedMapStokesU = MapStokesU.copy()
+
+######## taking points only with singal to noise ratio more than 2
+MapPolSNR.data[:] = np.nan
+MapPolSNR.data = MapPolFlux.data/MapPolFluxError.data
+Selector = (MapPolSNR.data < 2)
+
+BlankedMapPol.data[Selector] = np.nan
+BlankedMapPolAngle.data[Selector] = np.nan
+BlankedMapStokesI.data[Selector] = np.nan
+BlankedMapStokesQ.data[Selector] = np.nan
+BlankedMapStokesU.data[Selector] = np.nan
+
+############## generating the RA and DEC mesh
 DEC_grid,RA_grid = generate_RA_DEC_mesh(hdul[0])
-# # print(1.5*0.5*(DEC_grid[1,0] - DEC_grid[0,0])*60)
+seperation = MapPolAngle.copy()
 
-# # plt.figure()
-# # ax1 = plt.subplot(121)
-# # ax1.imshow(DEC_grid)
-# # ax2 = plt.subplot(122)
-# # ax2.imshow(RA_grid)
-# # plt.show()
 
-s_map = np.zeros_like(Pol_angle)
-seperation = Calc_l(RA_grid[50,50],DEC_grid[50,50],RA_grid,DEC_grid)
-seperation = seperation*(seperation<1.5*0.5)*(0.5*0.5<seperation)
-plt.imshow(seperation)
-plt.show()
-Pol_mask = Pol_angle*(seperation>0)
-plt.imshow(Pol_mask)
-plt.show()
-Pol_diff =  ((Pol_angle-Pol_angle[50,50])**2)*(seperation>0)
-plt.imshow(Pol_diff)
-plt.show()
-S = np.sqrt(np.nansum(Pol_diff)/np.count_nonzero(Pol_diff))
+############## Testing the algorithm at point x_index,y_index
+
+x_index = 50
+y_index = 50
+
+############## making the filter for selecting points withing the ring
+seperation.data = Calc_l(RA_grid[x_index,y_index],DEC_grid[x_index,y_index],RA_grid,DEC_grid)
+seperation_selector = (seperation.data<0.5*0.5)
+seperation.data[seperation_selector] = np.nan
+seperation_selector = (seperation.data>1.5*0.5)
+seperation.data[seperation_selector] = np.nan
+
+
+############## first version
+AngleDiff = BlankedMapPolAngle.data - BlankedMapPolAngle.data[x_index,y_index]
+Angle_selector =AngleDiff>90
+AngleDiff[Angle_selector] = AngleDiff[Angle_selector] - 180
+Angle_selector = AngleDiff<-90
+AngleDiff[Angle_selector] = AngleDiff[Angle_selector] + 180
+seperation_selector = (seperation.data >0)
+S = np.nanmean(AngleDiff[seperation_selector]**2)**0.5
 print(S)
-s_map[50,50] = S
 
 
-s_map = np.zeros_like(Pol_angle)
+############ second version
+tempa = BlankedMapStokesQ.data*BlankedMapStokesU.data[x_index,y_index] - BlankedMapStokesQ.data[x_index,y_index]*BlankedMapStokesU.data
+tempb = BlankedMapStokesQ.data*BlankedMapStokesQ.data[x_index,y_index] + BlankedMapStokesU.data*BlankedMapStokesU.data[x_index,y_index]
+
+AngleDiff_v2 = (180/np.pi)*0.5*np.arctan(tempa/tempb)
+Angle_selector_v2 =AngleDiff_v2>90
+AngleDiff_v2[Angle_selector_v2] = AngleDiff_v2[Angle_selector_v2] - 180
+Angle_selector_v2 = AngleDiff_v2<-90
+AngleDiff_v2[Angle_selector_v2] = AngleDiff_v2[Angle_selector_v2] + 180
+S2 = np.nanmean(AngleDiff_v2[seperation_selector]**2)**0.5
+print(S2)
+
+
+########## Running the algorithm on each cell
+S_map = BlankedMapPolAngle.copy()
+S_map_v2 = BlankedMapPolAngle.copy()
 for i in range(RA_grid.shape[0]):
     for j in range(RA_grid.shape[1]):
-        seperation = Calc_l(RA_grid[i,j],DEC_grid[i,j],RA_grid,DEC_grid)
-        seperation = seperation*(seperation<1.5*0.5)*(0.5*0.5<seperation)
-        # plt.imshow(seperation)
-        # plt.show()
-        Pol_mask = Pol_angle*(seperation>0)
-        # plt.imshow(Pol_mask)
-        # plt.show()
-        Pol_diff =  ((Pol_angle- Pol_angle[i,j])**2)*(seperation>0)
-        # plt.imshow(Pol_diff)
-        # plt.show()
-        S = np.sqrt(np.nansum(Pol_diff)/np.count_nonzero(Pol_diff))
-        s_map[i,j] = S
+
+        ##### seperation filter
+        seperation.data = Calc_l(RA_grid[i,j],DEC_grid[i,j],RA_grid,DEC_grid)
+        seperation_selector = (seperation.data<0.5*0.5)
+        seperation.data[seperation_selector] = np.nan
+        seperation_selector = (seperation.data>1.5*0.5)
+        seperation.data[seperation_selector] = np.nan
+
+        ##### first version
+        AngleDiff = BlankedMapPolAngle.data - BlankedMapPolAngle.data[i,j]
+        Angle_selector =AngleDiff>90
+        AngleDiff[Angle_selector] = AngleDiff[Angle_selector] - 180
+        Angle_selector = AngleDiff<-90
+        AngleDiff[Angle_selector] = AngleDiff[Angle_selector] + 180
+        seperation_selector = (seperation.data >0)
+        S = np.nanmean(AngleDiff[seperation_selector]**2)**0.5
+        S_map.data[i,j] = S
+
+        ##### second version
+        tempa = BlankedMapStokesQ.data*BlankedMapStokesU.data[i,j] - BlankedMapStokesQ.data[i,j]*BlankedMapStokesU.data
+        tempb = BlankedMapStokesQ.data*BlankedMapStokesQ.data[i,j] + BlankedMapStokesU.data*BlankedMapStokesU.data[i,j]
+        AngleDiff_v2 = 0.5*np.arctan(tempa/tempb)
+        Angle_selector_v2 =AngleDiff_v2>90
+        AngleDiff_v2[Angle_selector_v2] = AngleDiff_v2[Angle_selector_v2] - 180
+        Angle_selector_v2 = AngleDiff_v2<-90
+        AngleDiff_v2[Angle_selector_v2] = AngleDiff_v2[Angle_selector_v2] + 180
+        S_v2 = np.nanmean(AngleDiff_v2[seperation_selector]**2)**0.5
+        S_map_v2.data[i,j] = S_v2
 
 
-
-################### 1st attempt at plotting  S vs P ###################################
-# Pol = hdul[7].data
-# s_map = s_map + Pol*(Pol == np.nan)
-# Pol_mod = Pol*(Pol<20)
 # plt.figure()
 # ax1 = plt.subplot(121)
-# ax1.set_title('Pol [%]')
-# ax1.set_xlabel('RA')
-# ax1.set_ylabel('DEC')
-# ax1.imshow(Pol_mod,origin='lower')
+# ax1.imshow(S_map.data)
 # ax2 = plt.subplot(122)
-# ax2.imshow(s_map,origin='lower')
-# ax2.set_title('S [deg]')
-# ax2.set_xlabel('RA')
-# ax2.set_ylabel('DEC')
+# ax2.imshow(S_map_v2.data)
 # plt.show()
 
-# s_map_array = s_map.flatten()
-# Pol_array = Pol_mod.flatten()
-# new_s_map_array = [x for x in s_map_array if np.isnan(x) == False]
-# new_Pol_array = [x for x in Pol_array if np.isnan(x) == False]
 
-# x_min = np.min(new_Pol_array)
-# x_max = np.max(new_Pol_array)
-  
-# y_min = np.min(new_s_map_array)
-# y_max = np.max(new_s_map_array)
-  
-# x_bins = np.linspace(x_min, x_max, 50)
-# y_bins = np.linspace(y_min, y_max, 50)
+s_map_array = S_map.data.flatten()
+Pol_array = BlankedMapPol.data.flatten()
 
-# fig = plt.subplots(figsize =(10, 10))
-# # Creating plot
-# plt.hist2d(new_Pol_array, new_s_map_array,bins =[x_bins, y_bins])
-# plt.title("S X P 2D histogram")
-# plt.ylabel('S [deg]')
-# plt.xlabel('P [%]')
+y_min = np.nanmin(Pol_array)
+y_max = 20
   
-# # show plot
-# plt.show()
+x_min = np.nanmin(s_map_array)
+x_max = np.nanmax(s_map_array)
+  
+x_bins = np.arange(x_min, x_max, 0.5)
+y_bins = np.arange(y_min, y_max, 0.5)
+
+fig = plt.subplots(figsize =(10, 10))
+# Creating plot
+plt.hist2d(s_map_array,Pol_array,bins =[x_bins, y_bins])
+plt.title("P X S 2D histogram")
+plt.ylabel('P [%]')
+plt.xlabel('S [deg]')
+plt.show()
+
+
+I_map_array = BlankedMapStokesI.data.flatten()
+
+y_min = np.nanmin(Pol_array)
+y_max = 20
+  
+x_min = np.nanmin(I_map_array)
+x_max = np.nanmax(I_map_array)
+  
+x_bins = np.arange(x_min, x_max, 1)
+y_bins = np.arange(y_min, y_max, 0.25)
+
+fig = plt.subplots(figsize =(10, 10))
+# Creating plot
+plt.hist2d(I_map_array,Pol_array,bins =[x_bins, y_bins])
+plt.title("P X I 2D histogram")
+plt.ylabel('P [%]')
+plt.xlabel('I ')
+plt.show()
+
+
+
+# ################### 1st attempt at plotting  S vs P ###################################
+# # Pol = hdul[7].data
+# # s_map = s_map + Pol*(Pol == np.nan)
+# # Pol_mod = Pol*(Pol<20)
+# # plt.figure()
+# # ax1 = plt.subplot(121)
+# # ax1.set_title('Pol [%]')
+# # ax1.set_xlabel('RA')
+# # ax1.set_ylabel('DEC')
+# # ax1.imshow(Pol_mod,origin='lower')
+# # ax2 = plt.subplot(122)
+# # ax2.imshow(s_map,origin='lower')
+# # ax2.set_title('S [deg]')
+# # ax2.set_xlabel('RA')
+# # ax2.set_ylabel('DEC')
+# # plt.show()
+
+# # s_map_array = s_map.flatten()
+# # Pol_array = Pol_mod.flatten()
+# # new_s_map_array = [x for x in s_map_array if np.isnan(x) == False]
+# # new_Pol_array = [x for x in Pol_array if np.isnan(x) == False]
+
+# # x_min = np.min(new_Pol_array)
+# # x_max = np.max(new_Pol_array)
+  
+# # y_min = np.min(new_s_map_array)
+# # y_max = np.max(new_s_map_array)
+  
+# # x_bins = np.linspace(x_min, x_max, 50)
+# # y_bins = np.linspace(y_min, y_max, 50)
+
+# # fig = plt.subplots(figsize =(10, 10))
+# # # Creating plot
+# # plt.hist2d(new_Pol_array, new_s_map_array,bins =[x_bins, y_bins])
+# # plt.title("S X P 2D histogram")
+# # plt.ylabel('S [deg]')
+# # plt.xlabel('P [%]')
+  
+# # # show plot
+# # plt.show()
